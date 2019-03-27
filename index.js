385748e2e7340a0e96b53a2b1f4e23ed21e596a8
youneshck <42220507+youneshck@users.noreply.github.com>
2019-03-27 19:17:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ api.stockX.url = (url, cb) => {
 
                 discordFields.push({
                     name: "US " + tempBody.children[size].shoeSize,
-                    value: "$" + tempBody.children[size].market.highestBid,
+                    value: "$" + tempBody.children[size].market.highestBid "- €" + tempBody.children[size].market.highestBid*0.89,
                     inline: true
                 });
             }
