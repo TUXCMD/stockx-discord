05f1c3ff1815b9564674953e9eb15e6a3c747bb7
youneshck <42220507+youneshck@users.noreply.github.com>
2019-03-27 19:10:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -104,105 +104,14 @@ api.stockX.url = (url, cb) => {
             productObj.market.totalSold = tempBody.market.deadstockSold;
             productObj.market.averagePrice = "$" + tempBody.market.averageDeadstockPrice;
             
-var https = require('https');
 
-eurL = function convertCurrency('tempBody.market.lowestAsk', 'USD', 'EUR'); {
-  var apiKey = 'bbb7d17af39d5eb35fc2';
-
-  fromCurrency = encodeURIComponent(fromCurrency);
-  toCurrency = encodeURIComponent(toCurrency);
-  var query = fromCurrency + '_' + toCurrency;
-
-  var url = 'https://api.currencyconverterapi.com/api/v6/convert?q='
-            + query + '&compact=ultra&apiKey=' + apiKey;
-
-  https.get(url, function(res){
-      var body = '';
-
-      res.on('data', function(chunk){
-          body += chunk;
-      });
-
-      res.on('end', function(){
-          try {
-            var jsonObj = JSON.parse(body);
-
-            var val = jsonObj[query];
-            if (val) {
-              var total = val * amount;
-              cb(null, Math.round(total * 100) / 100);
-            } else {
-              var err = new Error("Value not found for " + query);
-              console.log(err);
-              cb(err);
-            }
-          } catch(e) {
-            console.log("Parse error: ", e);
-            cb(e);
-          }
-      });
-  }).on('error', function(e){
-        console.log("Got an error: ", e);
-        cb(e);
-  });
-}
-
-//uncomment to test
-/*
-convertCurrency(10, 'USD', 'PHP', function(err, amount) {
-  console.log(amount);
-});
-*/            
-            
-         eurB = function convertCurrency('tempBody.market.lowestAsk', 'USD', 'EUR'); {
-  var apiKey = 'bbb7d17af39d5eb35fc2';
-
-  fromCurrency = encodeURIComponent(fromCurrency);
-  toCurrency = encodeURIComponent(toCurrency);
-  var query = fromCurrency + '_' + toCurrency;
-
-  var url = 'https://api.currencyconverterapi.com/api/v6/convert?q='
-            + query + '&compact=ultra&apiKey=' + apiKey;
-
-  https.get(url, function(res){
-      var body = '';
-
-      res.on('data', function(chunk){
-          body += chunk;
-      });
-
-      res.on('end', function(){
-          try {
-            var jsonObj = JSON.parse(body);
-
-            var val = jsonObj[query];
-            if (val) {
-              var total = val * amount;
-              cb(null, Math.round(total * 100) / 100);
-            } else {
-              var err = new Error("Value not found for " + query);
-              console.log(err);
-              cb(err);
-            }
-          } catch(e) {
-            console.log("Parse error: ", e);
-            cb(e);
-          }
-      });
-  }).on('error', function(e){
-        console.log("Got an error: ", e);
-        cb(e);
-  });
-}
-            
-            
-            
-            
-            
-            
-            
-            
-            
+        //usd to eur
+            var x = tempBody.market.lowestAsk;
+            var y = tempBody.market.highestBid;
+           var cc = 0.89;
+            var lowest = x*cc;
+            var high = y*cc;
+     
 
             let discordFields = [{
                 name: "Retail Price",
@@ -226,12 +135,12 @@ convertCurrency(10, 'USD', 'PHP', function(err, amount) {
             },
             {
                 name: "Lowest Ask",
-                value: `${productObj.market.lowestAsk} - ${eurL}`,
+                value: `${productObj.market.lowestAsk} - € ${lowest}`,
                 inline: true
             },
             {
                 name: "Highest Bid",
-                value: `${productObj.market.highestBid} - ${eurB}`,
+                value: `${productObj.market.highestBid} - € ${high}`,
                 inline: true
             },
             {
